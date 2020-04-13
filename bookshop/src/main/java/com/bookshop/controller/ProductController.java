3a69931674b84ee8c356ce3b541e649d05198c50
nhatquang99 <vhnhatquang7399@gmail.com>
2020-04-13 14:34:27 +0700

    Overflow Fix & Display Product Detail

--- bookshop/src/main/java/com/bookshop/controller/ProductController.java
+++ bookshop/src/main/java/com/bookshop/controller/ProductController.java
@@ -9,31 +9,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.bookshop.dao.CategoryDAO;
 import com.bookshop.dao.ProductDAO;
-import com.bookshop.entity.Category;
 import com.bookshop.entity.Product;
 
 @Controller
 public class ProductController {
 	@Autowired
 	ProductDAO pdao;
+
 	@RequestMapping("product/list-by-category/{cid}")
-	public String listByCategory(Model model,
-			@PathVariable("cid") Integer categoryId) {
-		List<Product> list=pdao.findByCategoryId(categoryId);
-		model.addAttribute("list",list);
+	public String listByCategory(Model model, @PathVariable("cid") Integer categoryId) {
+		List<Product> list = pdao.findByCategoryId(categoryId);
+		model.addAttribute("list", list);
 		return "product/list";
 	}
-	
+
 	@RequestMapping("product/list-by-keywords")
-	public String listByKeywords(Model model,
-			@RequestParam("keywords") String keywords) {
-		List<Product> list=pdao.findByKeywords(keywords);
-		model.addAttribute("list",list);
+	public String listByKeywords(Model model, @RequestParam("keywords") String keywords) {
+		List<Product> list = pdao.findByKeywords(keywords);
+		model.addAttribute("list", list);
 		return "product/list";
 	}
-	
-	
+
+	@RequestMapping("product/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Product prod = pdao.findById(id);
+		List<Product> list = pdao.findByCategoryId(prod.getCategory().getId());
+		model.addAttribute("prod", prod);
+		model.addAttribute("list", list);
+		return "product/detail";
+	}
 
 }
--- bookshop/src/main/webapp/WEB-INF/views/product/list.jsp
+++ bookshop/src/main/webapp/WEB-INF/views/product/list.jsp
@@ -4,20 +4,20 @@
 <c:forEach var="p" items="${list}"> 
 	<div class="col-sm-4">
 		<div class="thumbnail">
-      		<a href="/w3images/lights.jpg">
+      		<a href="/product/detail/${p.id}">
         		<img class="bookshop-prod" src="/static/products/${p.image}">
         	</a>   
-        	<div class="caption"> 
-         		<p>${p.name}</p>
-       			<div class="pull-right">
+        	<div class="caption">   
+     			<p class="overflow-fix">${p.name}</p>
+        		<div class="pull-right">
          			<button class="btn btn-sm btn-danger">
          				<i class="glyphicon glyphicon-shopping-cart"></i>
          			</button>
          			<button class="btn btn-sm btn-warning">
          				<i class="glyphicon glyphicon-star"></i>
          			</button>
-       			</div>     
-       			<p>${p.unitPrice}</p>
+       			</div>  
+ 				<p>${p.unitPrice}</p>
        		</div>
        		 	
 		</div>
